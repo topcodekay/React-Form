611c767acab84542a2b48a484094e6f81227f38d
Kolawole Kayode <mightybalogun@gmail.com>
2022-08-08 07:04:19 +0100

    first commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import ExpenseDate from "../Expenses/ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated");
+    console.log("clicked");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Tile</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
