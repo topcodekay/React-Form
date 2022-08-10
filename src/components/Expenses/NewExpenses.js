bcac098e2cc47f15da54d32ce10593bfcc52e2f8
Kolawole Kayode <mightybalogun@gmail.com>
2022-08-10 02:56:59 +0100

    update build for gh-pages

--- src/components/Expenses/NewExpenses.js
+++ src/components/Expenses/NewExpenses.js
@@ -1,31 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 function NewExpenses(props) {
+  const [pickedYear, selectYear] = useState("2021");
+  const filterSelector = (yearPicked) => {
+    selectYear(yearPicked);
+  };
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-        title={props.items[3].title}
-      />
+      <ExpensesFilter select={pickedYear} onChangeFilter={filterSelector} />
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }
