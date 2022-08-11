1f2d6f0703e0a0828474717acd9d1ed19b343978
Kolawole Kayode <mightybalogun@gmail.com>
2022-08-11 07:49:14 +0100

    Deployed website

--- src/components/Expenses/NewExpenses.js
+++ src/components/Expenses/NewExpenses.js
@@ -3,23 +3,21 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function NewExpenses(props) {
   const [pickedYear, selectYear] = useState("2021");
   const filterSelector = (yearPicked) => {
     selectYear(yearPicked);
   };
+  const filteredExpense = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === pickedYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter select={pickedYear} onChangeFilter={filterSelector} />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesList items={filteredExpense} />
     </Card>
   );
 }
